10abf20783ae3daf72a1c2a233bf878fb64b1602
Nadya Frolova <n.frolova.kzn@yandex.ru>
2023-01-08 21:04:24 +0300

    внесены изменения для имитации конфликта

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static jdk.internal.logger.DefaultLoggerFinder.SharedLoggers.system;
 
 public class NewTest {
     @Test
@@ -10,5 +11,6 @@ public class NewTest {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
+        System.out.println("внесены изменения для имитации конфликта");
     }
 }
